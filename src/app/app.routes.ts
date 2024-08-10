54f75fa94a0eb4c4d38b798613d27d47f3db49bb
trileafu <qeis926@gmail.com>
2024-08-10 09:08:57 +0700

    routing sederhana

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { DashboardPage } from './dashboard/dashboard.page';
+import { LoginPage } from './login/login.page';
+import { RegisterPage } from './register/register.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DashboardPage,
+  },
+  {
+    path: 'login',
+    component: LoginPage,
+  },
+  {
+    path: 'register',
+    component: RegisterPage,
+  },
+];
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.page.html',
+  styleUrl: './login.page.css',
+})
+export class LoginPage {}
